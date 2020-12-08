a25f93a8ba712fe9d824ccd1b9c229ae98678329
sTiKyt <stikyt@protonmail.com>
2020-12-08 18:50:10 +0200

    Added cache timeout, timeout and url arguments for argparser and replaced static variables with if statements

--- polybar_onlinestatus/polybar_onlinestatus.py
+++ polybar_onlinestatus/polybar_onlinestatus.py
@@ -4,12 +4,8 @@ from time import sleep
 
 
 class OnlineStatus:
-    """"""
     def __init__(self):
-        self.url = "https://google.com"
         self.parser = argparse.ArgumentParser()
-        self.cache_timeout = 10
-        self.timeout = 2
         self.offline_icon = ""
         self.online_icon = ""
         self.color = {
@@ -19,13 +15,17 @@ class OnlineStatus:
         }
         self.parser.add_argument("--boolean-mode", dest="boolean_mode", help="Returns boolean value if True, network icon if False", action="store_true")
         self.parser.add_argument("--color-mode", dest="color_mode", help="Sets color to result if True", action="store_true")
-
+        self.parser.add_argument("-ct", "--cache-timeout", type=int, metavar="REFRESH_RATE", dest="cache_timeout", help="Specify cache refresh rate in seconds, defaults to 5")
+        self.parser.add_argument("-ut", "--url-timeout", type=int, metavar="TIMEOUT", dest="url_timeout", help="Specify URL ping timeout in seconds, defaults to 2")
+        self.parser.add_argument("-l", "--link", "--url", type=str, metavar="URL", dest="url", help="Specify URL that will be used to verify connection, defaults to 'https://google.com'")
         self.args = self.parser.parse_args()
+        self.cache_timeout = self.args.cache_timeout if self.args.cache_timeout is not None else 5
+        self.timeout = self.args.url_timeout if self.args.url_timeout is not None else 2
+        self.url = self.args.url if self.args.url is not None else "https://google.com"
         if self.args.boolean_mode:
             self.boolean_mode = True
         else:
             self.boolean_mode = False
-        self.args = self.parser.parse_args()
         if self.args.color_mode:
             self.color_mode = True
         else:
@@ -81,4 +81,5 @@ def main():
         return OnlineStatus().online_result()
 
 
-# print(main()) # For testing purposes
+# print(main())  # For testing purposes
+
