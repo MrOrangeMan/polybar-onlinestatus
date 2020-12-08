2b4df0a006f2ee00749d2bebebad6b3ac142c3f4
sTiKyt <stikyt@protonmail.com>
2020-12-08 21:04:56 +0200

    Made colorcodes shorter

--- polybar_onlinestatus/__init__.py
+++ polybar_onlinestatus/__init__.py
@@ -1,1 +1,1 @@
-__version__ = "0.1a2.dev7"
+__version__ = "0.1a2.dev8"
--- polybar_onlinestatus/polybar_onlinestatus.py
+++ polybar_onlinestatus/polybar_onlinestatus.py
@@ -10,8 +10,8 @@ class OnlineStatus:
         self.online_icon = "  "
         self.color = {
             "end": "%{F-}",
-            "offline": "%{F#ff0000}",
-            "online": "%F{#008000}"
+            "offline": "%{F#f00}",
+            "online": "%F{#080}"
         }
         self.parser.add_argument("-bm", "--boolean-mode", dest="boolean_mode", help="Returns boolean value if True, network icon if False", action="store_true")
         self.parser.add_argument("-cm", "--color-mode", dest="color_mode", help="Sets color to result if True", action="store_true")
